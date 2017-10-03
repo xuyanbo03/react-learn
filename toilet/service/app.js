41027ffb4d2d47a26468cd010d6abb6108ca4814
xuyanbo03 <610958401@qq.com>
2017-10-03 15:45:55 +0800

    update
    后台完善路由和页面

--- toilet/service/app.js
+++ toilet/service/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var session=require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var data=require('./routes/data');
 
 var app = express();
@@ -30,8 +29,7 @@ app.use(session({
     saveUninitialized:true
 }));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
 app.use('/data',data);
 
 // catch 404 and forward to error handler
